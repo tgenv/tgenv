f1ab88d5cd92b7ba05336aca1f6e4a510491ba83
Simon Merrick <s.m3rrick@gmail.com>
2023-01-11 15:20:18 +1300

    test tgenv list with no versions

--- test/test_list.sh
+++ test/test_list.sh
@@ -19,29 +19,23 @@ source $(dirname $0)/helpers.sh \
 echo "### List local versions"
 cleanup || error_and_die "Cleanup failed?!"
 
-for v in 0.38.12 0.37.4 0.36.11 0.33.0 0.29.7; do
-  tgenv install ${v} || error_and_proceed "Install of version ${v} failed"
-done
+# for v in 0.38.12 0.37.4 0.36.11 0.33.0 0.29.7; do
+#   tgenv install ${v} || error_and_proceed "Install of version ${v} failed"
+# done
 
 # NOTE(iokiwi): Hardcoding the expected output like this may work for the github action
 # but will probably fail if run locally. I believe there is an example of the output 
 # being generated more dynamically in the tfenv project that we could 'borrow'.
 result="$(tgenv list)";
-expected="$(cat << EOS
-  0.38.12
-  0.37.4
-  0.36.11
-  0.33.0
-* 0.29.7 (set by /home/runner/work/tgenv/tgenv/version)
-EOS
-)"
-
-# echo "---- BEGIN DEBUG INFO ----"
-# uname -a
-# echo "${BASH_VERSION}"
-# echo "${expected}"
-# echo "${result}"
-# echo "---- END DEBUG INFO ----"
+expected=""
+# expected="$(cat << EOS
+#   0.38.12
+#   0.37.4
+#   0.36.11
+#   0.33.0
+# * 0.29.7 (set by /home/runner/work/tgenv/tgenv/version)
+# EOS
+# )"
 
 [ "${expected}" == "${result}" ] \
   || error_and_proceed "List mismatch.\nExpected:\n${expected}\nGot:\n${result}"
