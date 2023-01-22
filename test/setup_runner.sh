72ae8632e82134848635325eabfa6e1175723e34
Simon Merrick <s.m3rrick@gmail.com>
2023-01-22 22:52:22 +1300

    Run shellcheck over and apply recomendations

--- test/setup_runner.sh
+++ test/setup_runner.sh
@@ -1,16 +1,21 @@
 #!/usr/bin/env sh
 
+if [ "$(uname)" = 'Darwin' ]; then
+  echo "Skipping for MacOS"
+  exit 0
+fi;
+
 . /etc/os-release
-if [[ "${ID}" == "alpine" ]]; then
-    apk add bash git
+if [ "${ID}" = "alpine" ]; then
+    apk add bash git curl
 fi
 
-if [[ "${ID}" == "ubuntu" ]]; then
+if [ "${ID}" = "ubuntu" ]; then
     apt update -y
     apt install -y git
 fi
 
-if [[ "${ID}" == "fedora" ]]; then
+if [ "${ID}" = "fedora" ]; then
     yum update -y
     yum install git -y
 fi
