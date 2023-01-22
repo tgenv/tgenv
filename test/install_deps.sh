52592adaf158721e54d00d7593d6ce24bb34b14a
Simon Merrick <s.m3rrick@gmail.com>
2023-01-22 13:57:47 +1300

    Install dependencies for tests

--- test/install_deps.sh
+++ test/install_deps.sh
@@ -3,4 +3,20 @@ set -uo pipefail;
 
 if [[ $(uname) == 'Darwin' ]] && [ $(which brew) ]; then
   brew install grep;
+  exit
 fi;
+
+. /etc/os-relase
+if [[ "${ID}" == "alpine" ]]; then
+    apk add bash git
+fi
+
+if [[ "${ID}" == "ubuntu" ]]; then
+    apt update -y
+    apt install -y git
+fi
+
+if [[ "${ID}" == "fedora" ]]; then
+    yum update -y
+    yum install git -y
+fi
