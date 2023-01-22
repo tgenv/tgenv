38012ac6c2f0cbb3586c91d962893499925d16a0
Simon Merrick <s.m3rrick@gmail.com>
2023-01-22 22:43:34 +1300

    Use sh to setup runner

--- test/install_deps.sh
+++ test/install_deps.sh
@@ -5,18 +5,3 @@ if [[ $(uname) == 'Darwin' ]] && [ $(which brew) ]; then
   brew install grep;
   exit
 fi;
-
-. /etc/os-release
-if [[ "${ID}" == "alpine" ]]; then
-    apk add bash git
-fi
-
-if [[ "${ID}" == "ubuntu" ]]; then
-    apt update -y
-    apt install -y git
-fi
-
-if [[ "${ID}" == "fedora" ]]; then
-    yum update -y
-    yum install git -y
-fi
